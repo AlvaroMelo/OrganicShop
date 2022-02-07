c151afe48e40975267cbc360a86b78d8adda18cc
Alvaro Melo <alvaroraposojunior@gmail.com>
2022-02-07 14:37:55 -0300

    Initial set up - Just adding some routes and deploying to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAq98JyukoNS2qX9CnkCkwEWtscOaccUIM",
+    authDomain: "awesome-organic-shop.firebaseapp.com",
+    projectId: "awesome-organic-shop",
+    storageBucket: "awesome-organic-shop.appspot.com",
+    messagingSenderId: "1097105165685",
+    appId: "1:1097105165685:web:512635e2b5784c4aeed73c",
+    measurementId: "G-S1BPH9VD8V"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAq98JyukoNS2qX9CnkCkwEWtscOaccUIM",
+    authDomain: "awesome-organic-shop.firebaseapp.com",
+    projectId: "awesome-organic-shop",
+    storageBucket: "awesome-organic-shop.appspot.com",
+    messagingSenderId: "1097105165685",
+    appId: "1:1097105165685:web:512635e2b5784c4aeed73c",
+    measurementId: "G-S1BPH9VD8V"
+  }
 };
 
 /*
