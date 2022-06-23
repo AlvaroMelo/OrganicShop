404ef99fd9ed7f958527e61492ffb6d748507b8d
Alvaro Melo <alvaroraposojunior@gmail.com>
2022-06-23 00:12:44 -0300

    Removing unnecessary code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyAq98JyukoNS2qX9CnkCkwEWtscOaccUIM",
-    authDomain: "awesome-organic-shop.firebaseapp.com",
-    projectId: "awesome-organic-shop",
-    storageBucket: "awesome-organic-shop.appspot.com",
-    messagingSenderId: "1097105165685",
-    appId: "1:1097105165685:web:512635e2b5784c4aeed73c",
-    measurementId: "G-S1BPH9VD8V"
-  }
+
 };
 
 /*
